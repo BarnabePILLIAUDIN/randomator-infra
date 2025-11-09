e957856550d28fc319ff8a2f1c727f39a08c0911
Barnabé PILLIAUDIN <barnabe.pilliaudin@gmail.com>
2025-11-09 13:20:20 +0100

    fix(argocd/local): add a port forward on argocd install script

--- argocd/install.sh
+++ argocd/install.sh
@@ -4,3 +4,10 @@ helm upgrade --install argocd argo/argo-cd \
   --values values.yaml \
   --namespace argocd \
   --create-namespace
+
+# Wait for agrgocd to be ready and create a port forward might not work locally due to ssl
+kubectl -n argocd wait --for=condition=available --timeout=120s deployment/argocd-server >/dev/null 2>&1 || {
+  echo "Warning: argocd-server deployment not ready yet, continuing to port-forward..."
+}
+echo "Forwarding local port 8080 -> argocd-server:443 (use Ctrl-C to stop)"
+kubectl -n argocd port-forward svc/argocd-server 8080:443
